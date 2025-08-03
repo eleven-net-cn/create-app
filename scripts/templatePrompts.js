bbdd321b7399ce0da3aca50517f35ff7d4a281ac
Eleven <master@eleven.net.cn>
2025-08-03 22:54:21 +0800

    perf: export package.json

--- scripts/templatePrompts.js
+++ scripts/templatePrompts.js
@@ -1,6 +1,23 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default {
-  // library
-  type: 'rollup',
+  // testing-library ☞ nr start
+  // type: 'rollup',
+  // packageManager: 'pnpm',
+  // installNow: false,
+
+  // testing-react-app ☞ nr start --react
+  type: 'create-next-app',
+  cwd: resolve(__dirname, '../.playground'),
   packageManager: 'pnpm',
   installNow: false,
+
+  // testing-vue-app ☞ nr start --vue
+  // type: 'create-nuxt',
+  // cwd: resolve(__dirname, '../.playground'),
+  // packageManager: 'pnpm',
+  // installNow: false,
 };
